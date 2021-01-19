95465a1f30c5b18fbfc6c1250a442d59f787a653
Park sejin <psj3205@gmail.com>
2021-01-19 17:41:19 +0900

    Update my code 3

--- basic_math.py
+++ basic_math.py
@@ -42,6 +42,6 @@ def get_median(number_list):
     if length % 2:
         median = number_list[length / 2]
     else:
-        median = number_list[length / 2 - 1] + number_list[length / 2] / 2
+        median = int(number_list[length / 2 - 1] + number_list[length / 2] / 2)
 
     return median
